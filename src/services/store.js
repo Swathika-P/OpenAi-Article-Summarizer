9f6262b1cc76d4edd3d6c514e438c93e85dea259
Swathika-P <swathika.p2021csec@sece.ac.in>
2024-08-17 12:20:00 +0530

    chore(store): enable Redux DevTools conditionally and optimize middleware setup

--- src/services/store.js
+++ src/services/store.js
@@ -6,5 +6,7 @@ export const store = configureStore({
     reducer: {
         [articleApi.reducerPath]: articleApi.reducer,
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware)
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware),
+    //Conditional Redux DevTools: This ensures that production build remains optimized without unnecessary development tools, enhancing performance
+    devTools: process.env.NODE_ENV !== 'production',
 })
